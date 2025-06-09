9773e49a601f04d5e680b39427e76ec6fe75dbaf
Owen Crook <crook.owen@gmail.com>
2025-06-08 22:21:03 -0700

    template page for professional experience

---
+++ app/professional-experience/page.tsx
@@ -0,0 +1,17 @@
+import { Box } from "@mantine/core"
+
+export default function ProfessionalExperiencePage() {
+  return (
+    <>
+      <Box style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+        <Box
+          pl={{ base: 8, sm: 16, md: 24 }}
+          pb={{ base: 8, sm: 16, md: 24 }}
+          style={{ flex: '0 0 auto' }}
+        >
+          <h1>Professional Experience</h1>
+        </Box>
+      </Box>
+    </>
+  )
+}
