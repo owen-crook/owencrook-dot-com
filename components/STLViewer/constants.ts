ca048898a6123be09ae46e10570a3b7689b01eb2
Owen Crook <crook.owen@gmail.com>
2025-07-10 20:15:30 -0700

    run prettier

--- components/STLViewer/constants.ts
+++ components/STLViewer/constants.ts
@@ -1,4 +1,4 @@
-import {ThreeDimensionalModel} from './types';
+import { ThreeDimensionalModel } from './types';
 
 export const DefaultThreeDimensionalModel: ThreeDimensionalModel = {
   label: 'Benchy',
@@ -10,18 +10,18 @@ export const DefaultThreeDimensionalModel: ThreeDimensionalModel = {
       description: 'The home page for benchy!',
     },
   ],
-  rotation: {x: -90, y: 0, z: 0},  // Default rotation values
+  rotation: { x: -90, y: 0, z: 0 }, // Default rotation values
 };
 
 export const modelData: ThreeDimensionalModel[] = [
-  DefaultThreeDimensionalModel, {
+  DefaultThreeDimensionalModel,
+  {
     label: 'TAG Graded Card Stand',
     file: 'tag-graded-card-holder',
     numberOfPhotos: 2,
     urls: [
       {
-        url:
-            'https://cad.onshape.com/documents/a40a6674b4066734936b17d0/w/122311ecbcadd44915ea1830/e/7a5ea9d34e1a60f42379e970?renderMode=0&uiState=68451d1a2185da1fe3f3daf6',
+        url: 'https://cad.onshape.com/documents/a40a6674b4066734936b17d0/w/122311ecbcadd44915ea1830/e/7a5ea9d34e1a60f42379e970?renderMode=0&uiState=68451d1a2185da1fe3f3daf6',
         label: 'OnShape',
         description: 'View the CAD file',
       },
@@ -31,7 +31,7 @@ export const modelData: ThreeDimensionalModel[] = [
         description: 'See the grade details!',
       },
     ],
-    rotation: {x: -90, y: 0, z: 0},  // Default rotation values
+    rotation: { x: -90, y: 0, z: 0 }, // Default rotation values
   },
   {
     label: 'MacBook Pro Cable Holder',
@@ -39,14 +39,12 @@ export const modelData: ThreeDimensionalModel[] = [
     numberOfPhotos: 4,
     urls: [
       {
-        url:
-            'https://cad.onshape.com/documents/db9752962b3e6c6fcb5759e8/w/0df4c88664c67aac8d8fa61f/e/3b6d5131e7c2100055bd242a',
+        url: 'https://cad.onshape.com/documents/db9752962b3e6c6fcb5759e8/w/0df4c88664c67aac8d8fa61f/e/3b6d5131e7c2100055bd242a',
         label: 'OnShape',
         description: 'View the CAD file',
       },
       {
-        url:
-            'https://en.wikipedia.org/wiki/MacBook_Pro_(Apple_silicon)#Technical_specifications_2',
+        url: 'https://en.wikipedia.org/wiki/MacBook_Pro_(Apple_silicon)#Technical_specifications_2',
         label: 'Macbook Pro Technical Specs',
       },
     ],
@@ -57,13 +55,12 @@ export const modelData: ThreeDimensionalModel[] = [
     numberOfPhotos: 0,
     urls: [
       {
-        url:
-            'https://cad.onshape.com/documents/3ea4a1be4b6595ed20435a03/w/e0bf52dc51a2830b8409992e/e/49a919ef0779289b7008eb7a?renderMode=0&uiState=687075e15838dc772d213d86',
+        url: 'https://cad.onshape.com/documents/3ea4a1be4b6595ed20435a03/w/e0bf52dc51a2830b8409992e/e/49a919ef0779289b7008eb7a?renderMode=0&uiState=687075e15838dc772d213d86',
         label: 'OnShape',
         description: 'View the CAD file',
       },
     ],
-    rotation: {x: 0, y: -180, z: 0},
+    rotation: { x: 0, y: -180, z: 0 },
   },
   {
     label: 'Honeycomb Lamp',
@@ -71,23 +68,21 @@ export const modelData: ThreeDimensionalModel[] = [
     numberOfPhotos: 3,
     urls: [
       {
-        url:
-            'https://cad.onshape.com/documents/bc576d8ebb6250084e0312f4/w/782304c94efb53f468da8de4/e/ab351bd39de305335975b117?renderMode=0&uiState=68707c859293c94e944be1d5',
+        url: 'https://cad.onshape.com/documents/bc576d8ebb6250084e0312f4/w/782304c94efb53f468da8de4/e/ab351bd39de305335975b117?renderMode=0&uiState=68707c859293c94e944be1d5',
         label: 'OnShape',
         description: 'View the CAD assembly',
       },
       {
         url: 'https://www.thingiverse.com/thing:4807723',
         label: 'Original Lamp',
-        description: 'By Bemko on Thingiverse'
+        description: 'By Bemko on Thingiverse',
       },
       {
-        url:
-            'https://www.amazon.com/dp/B08DXL1DZ3?ref_=ppx_hzsearch_conn_dt_b_fed_asin_title_7&th=1',
+        url: 'https://www.amazon.com/dp/B08DXL1DZ3?ref_=ppx_hzsearch_conn_dt_b_fed_asin_title_7&th=1',
         label: 'Hanging Lantern Cords',
-        description: 'Found on Amazon'
-      }
+        description: 'Found on Amazon',
+      },
     ],
-    rotation: {x: -90, y: 0, z: 0}
-  }
+    rotation: { x: -90, y: 0, z: 0 },
+  },
 ];
